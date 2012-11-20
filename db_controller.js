af1a2c9e987219fc7ac9dc503f4058b572e52aa2
kk1fff <kk1fff@patrickz.net>
2012-11-21 01:01:20 +0800

    Try parsing mongodb uri

--- db_controller.js
+++ db_controller.js
@@ -14,10 +14,28 @@ exports.getNumber = function(id) {
 
 // Connect to database.
 var initDb = function() {
-  var mongoUrl = process.env["MONGOLAB_URI"] ||
-    "mongodb://localhost/sushiaaa";
+  var mongoUrl = process.env["MONGOLAB_URI"];
 
-  console.log("Mongo URL: " + mongoUrl);
+  if (mongoUrl) {
+    console.log("DB: Mongo URL - " + mongoUrl);
+    // Parse URL
+    var reg = /(\w+):(\w+)@([\w\.]+)[:]?(\d*)\/(\w+)/;
+    var parsed = mongoUrl.match(reg);
+    var user = parsed[1];
+    var pass = parsed[2];
+    var host = parsed[3];
+    var port = parsed[4];
+    var db = parsed[5];
+
+    console.log("Mongo info:");
+    console.log("  user: " + user);
+    console.log("  pass: " + pass);
+    console.log("  host: " + host);
+    console.log("  port: " + port);
+    console.log("  db: " + db);
+  } else {
+    console.log("DB: Unable to get mongo uri");
+  }
 };
 
 initDb();
