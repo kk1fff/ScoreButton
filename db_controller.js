c895dccf2b796a587b4172d703706b042099eb7d
kk1fff <kk1fff@patrickz.net>
2012-11-20 23:06:39 +0800

    Change the environment variable for mongodb

--- db_controller.js
+++ db_controller.js
@@ -14,7 +14,7 @@ exports.getNumber = function(id) {
 
 // Connect to database.
 var initDb = function() {
-  var mongoUrl = process.env["MONGOHQ_URL"] ||
+  var mongoUrl = process.env["MONGOLAB_URI"] ||
     "mongodb://localhost/sushiaaa";
 
   console.log("Mongo URL: " + mongoUrl);
